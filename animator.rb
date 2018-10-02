da5ef52d1ca4ccb67ccb1ad5b68b31841cc9ce2a
Erik Eggers <erik.eggers@gmail.com>
2018-10-02 18:49:52 -0500

    separate responsibilities of led string and animator

---
+++ animator.rb
@@ -0,0 +1,86 @@
+class Animator
+
+  attr_reader :led_count
+  attr_accessor :led_string, :pattern, :current_frame
+
+  DEFAULT_OPTIONS = {
+    pattern: [{r:0, g:0, b:0}] # this could be improved...
+  }
+
+  def initialize led_string, options={}
+    options = DEFAULT_OPTIONS.merge options
+    self.led_string = led_string
+    self.pattern = options[:pattern]
+  end
+
+  def pattern= pattern
+    @frame_index = -1
+    @pattern = pattern
+    next_frame
+    nil
+  end
+
+  def led_string= led_string
+    @led_string = led_string
+    @led_count = @led_string.led_count
+  end
+
+  def next_frame
+    _next_frame :forward
+  end
+
+  def next_frame!
+    _next_frame! :forward
+  end
+
+  def previous_frame
+    _next_frame :backward
+  end
+
+  def previous_frame!
+    _next_frame! :backward
+  end
+
+  def display!
+    @led_string.leds = @current_frame
+    @led_string.sync!
+  end
+
+  def play!(fps=30)
+    loop(fps) do |_|
+      _.next_frame!
+    end
+  end
+
+  def rewind!(fps=30)
+    loop(fps) do |_|
+      _.previous_frame!
+    end
+  end
+
+  def loop(fps=30) # block
+    raise Error("Can't loop without block!") unless block_given?
+    delay = 1.0 / fps
+    while true
+      yield(self)
+      sleep delay
+    end
+  end
+
+  private
+
+  def _next_frame! direction
+    _next_frame direction
+    display!
+  end
+
+  def _next_frame direction
+    if direction == :forward
+      @frame_index = @frame_index + 1 % @pattern.length
+    elsif direction == :backward
+      @frame_index = @frame_index - 1 < 0 ? @pattern.length - 1 : @frame_index - 1
+    end
+
+    @current_frame = (@pattern * (@led_count * 1.0 / @pattern.length).ceil).rotate(@frame_index).first @led_count
+  end
+end
---
+++ led_string2.rb
@@ -0,0 +1,114 @@
+require 'rubyserial'
+
+class LedString2
+  attr_reader   :led_count,
+                :tty,
+                :baudrate,
+                :serial
+  
+  attr_accessor :leds, :verbose
+
+  DEFAULT_OPTIONS = {
+    led_count: 30,
+    tty: "/dev/cu.usbmodem14101",
+    baudrate: 115200,
+    verbose: false,
+    leds: []
+  }
+
+  def initialize options={}
+    options = DEFAULT_OPTIONS.merge options
+    @led_count = options[:led_count]
+    @tty       = options[:tty]
+    @baudrate  = options[:baudrate]
+    @pattern   = options[:pattern]
+    @repeat    = options[:repeat]
+    @verbose   = options[:verbose]
+
+    @serial = Serial.new @tty, @baudrate
+
+    # initialize the leds
+    self.leds = options[:leds]
+
+    # finally display the string
+    sync!
+  end
+
+  # if extras are supplied, ignore them, if not enough are supplied, fill with zeros
+  def leds= leds
+    @leds = leds.clone.fill(blank_led, leds.length..@led_count-1).first(@led_count)
+  end
+
+  def set_leds leds
+    self.leds = leds
+  end
+
+  def set_leds! leds
+    set_leds leds
+    sync!
+  end
+
+  def set_led n, led
+    @leds[n] = led
+  end
+
+  def set_led! n, led
+    set_led n, led
+    sync_single! n
+  end
+
+  def clear!
+    clear
+    sync!
+  end
+
+  def clear
+    self.leds= []
+  end
+
+
+  def blank_led
+    {r: 0, g: 0, b: 0}
+  end
+
+  #### SERIAL INTERFACE FUNCTIONS ###
+
+  # sync specific led to string
+  def sync_single index
+    rgb_data = @leds[index]
+    serial_write "#{index}:#{rgb_data[:r]},#{rgb_data[:g]},#{rgb_data[:b]};"
+  end
+
+  def sync_single! index
+    sync_single index
+    render!
+  end
+
+  # sync all leds to string
+  def sync
+    @leds.each_with_index {|_, index| sync_single index}
+  end
+
+  def sync!
+    sync
+    render!
+  end
+
+  # tell the led string to render
+  def render!
+    serial_write "render;"
+  end
+
+  # spit out the state as read from the LED string
+  def list
+    serial_write "list;"
+    puts @serial.read(100000)
+  end
+
+  def serial_write s
+    puts "serial_write: #{s}" if @verbose
+    @serial.write s
+    nil
+  end
+
+end
